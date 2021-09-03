dbf378279882550bc03a8db477f23124210bde37
Tastaturtaste <joshua.moellers@gmx.net>
2021-09-03 02:00:04 +0200

    Added new rocket model, fixed cost function, optimized clamp release

--- MPC_casadi.py
+++ MPC_casadi.py
@@ -1,130 +1,270 @@
 from casadi import *
 import time
 
+
 class MPC_Casadi:
-    def __init__(self,T_pred,dt,T_opt=-1):
+
+    def __init__(self, T_pred, dt, T_opt=-1):
+
         self.N_pred = int(T_pred / dt)
+
         if T_opt < 0:
+
             self.N_opt = self.N_pred
+
         else:
+
             self.N_opt = int(T_opt / dt)
-        self.ns = 4 # number of states
-        self.nu = 1 # number of inputs
-        self.np = 4 # number of parameters
+
+        self.ns = 4  # number of states
+
+        self.nu = 1  # number of inputs
+
+        self.np = 4  # number of parameters
 
         h = SX.sym('h')
+
         v = SX.sym('v')
+
         m = SX.sym('m')
+
         thrust = SX.sym('thrust')
-        x = vertcat(h,v,m,thrust)
+
+        x = vertcat(h, v, m, thrust)
+
         spoolup_time = SX.sym('spoolup_time')
+
         max_thrust = SX.sym('max_thrust')
+
         mass_loss = SX.sym('mass_loss')
-        grav = SX.sym('grav')
-        p = vertcat(spoolup_time,max_thrust,mass_loss,grav)
-        u = SX.sym('throttle') #between 0 and 100
+
+        g = SX.sym('g')
+
+        p = vertcat(spoolup_time, max_thrust,
+
+
+                    mass_loss, g)
+
+        u = SX.sym('throttle')  # between 0 and 100
+
         htarget = SX.sym('htarget')
-        vtarget = -2*grav*(h-htarget)
-        j = (v-vtarget)**2
+
+        h_delta = h - htarget
+
+        v_target_sq = -2*g*h_delta
+        j = (sign(v)*v*v-v_target_sq)**2 + h_delta**2
+
         #j = (h-htarget)**2
+
         hdot = v
-        vdot = (thrust / m) - grav
-        mdot = -thrust*mass_loss
+
+        vdot = (thrust / m) - g
+
+        mdot = -thrust/max_thrust * mass_loss
+
         thrustdot = (max_thrust * (u/100) - thrust) / spoolup_time
-        xdot = vertcat(hdot,vdot,mdot,thrustdot)
 
-        dae={'x':x,'p':vertcat(htarget,u,p),'ode':xdot,'quad':j}
+        xdot = vertcat(hdot, vdot, mdot, thrustdot)
 
-        X = MX.sym('X',x.sparsity())
-        P = MX.sym('P',p.sparsity())
-        U = MX.sym('U',u.sparsity())
+        dae = {'x': x, 'p': vertcat(htarget, u, p), 'ode': xdot, 'quad': j}
+
+        X = MX.sym('X', x.sparsity())
+
+        P = MX.sym('P', p.sparsity())
+
+        U = MX.sym('U', u.sparsity())
+
         TP = MX.sym('TP')
-        intg = integrator('intg','rk',dae,{'number_of_finite_elements':4,'expand':True})
-        F = intg(x0=X,p=vertcat(TP,U,P))
+
+        intg = integrator('intg', 'rk', dae, {
+
+
+                          'number_of_finite_elements': 4, 'expand': True})
+
+        F = intg(x0=X, p=vertcat(TP, U, P))
+
         xf = F['xf']
+
         qf = F['qf']
-        f = Function('f',[X,TP,U,P],[xf,qf],['x0','TP','U','P'],['xf','qf'])
+
+        f = Function('f', [X, TP, U, P], [xf, qf], [
+
+
+                     'x0', 'TP', 'U', 'P'], ['xf', 'qf'])
 
         g = []
+
         self.lbg = []
+
         self.ubg = []
+
         J = 0
-        lbx=[0,-inf,0,0]
-        ubx=[inf,inf,inf,inf]
-        lbu=[0]
-        ubu=[100]
-        X=MX.sym('X',self.ns,self.N_pred+1)
-        
-        U_opt=MX.sym('U_opt',self.nu,self.N_opt)
-        U_hold = MX.sym('U_hold',self.nu,self.N_pred-self.N_opt)
-        U = horzcat(U_opt,U_hold)
 
-        X0=MX.sym('X0',self.ns)
-        P = MX.sym('P',self.np)
-        TP = MX.sym('TP',self.N_pred)
-        Xk = X[:,0]
-        g+=[Xk - X0]
+        lbx = [0, -inf, 0, 0]
+
+        ubx = [inf, inf, inf, inf]
+
+        lbu = [0]
+
+        ubu = [100]
+
+        X = MX.sym('X', self.ns, self.N_pred+1)
+
+        U_opt = MX.sym('U_opt', self.nu, self.N_opt)
+
+        U_hold = MX.sym('U_hold', self.nu, self.N_pred-self.N_opt)
+
+        U = horzcat(U_opt, U_hold)
+
+        X0 = MX.sym('X0', self.ns)
+
+        P = MX.sym('P', self.np)
+
+        TP = MX.sym('TP', self.N_pred)
+
+        Xk = X[:, 0]
+
+        g += [Xk - X0]
+
         self.lbg += [DM.zeros(Xk.sparsity())]
+
         self.ubg += [DM.zeros(Xk.sparsity())]
+
         # Setup prediction horizon >= opt horizon
-        g+=[reshape(U_hold,U_hold.numel(),1) - reshape(U_opt[:,-1],self.nu,1)]
-        self.lbg+=[DM.zeros(U_hold.numel())]
-        self.ubg+=[DM.zeros(U_hold.numel())]
+
+        g += [reshape(U_hold, U_hold.numel(), 1) -
+
+
+              reshape(U_opt[:, -1], self.nu, 1)]
+
+        self.lbg += [DM.zeros(U_hold.numel())]
+
+        self.ubg += [DM.zeros(U_hold.numel())]
+
         for k in range(self.N_pred):
+
             # New NLP variable for the control
-            Uk = U[:,k]
-            #Target Point for this timestep
-            TPk=TP[k]
+
+            Uk = U[:, k]
+
+            # Target Point for this timestep
+
+            TPk = TP[k]
+
             # Integrate till the end of the interval
-            Fk = f(x0=Xk, TP=TPk,U=Uk,P=P)
+
+            Fk = f(x0=Xk, TP=TPk, U=Uk, P=P)
+
             Xk_end = Fk['xf']
-            J=J+Fk['qf']
+
+            J = J+Fk['qf']
+
             # New NLP variable for state at end of interval
-            Xk = X[:,k+1]
+
+            Xk = X[:, k+1]
+
             # Add equality constraint
-            g   += [Xk_end-Xk]
+
+            g += [Xk_end-Xk]
+
             self.lbg += [DM.zeros(Xk.sparsity())]
+
             self.ubg += [DM.zeros(Xk.sparsity())]
-        
+
         self.lbg = vertcat(*self.lbg)
+
         self.ubg = vertcat(*self.ubg)
+
         opt_var = vertcat(
-            reshape(X,X.numel(),1),
-            reshape(U,U.numel(),1))
-        prob = {'f': J, 'x': opt_var, 'g': vertcat(*g),'p':vertcat(X0,P,TP)}
+
+
+            reshape(X, X.numel(), 1),
+
+
+            reshape(U, U.numel(), 1))
+
+        prob = {'f': J, 'x': opt_var, 'g': vertcat(
+
+
+            *g), 'p': vertcat(X0, P, TP)}
+
         if True:
-            ipopt_opts = {'ipopt':{'max_iter':30,'warm_start_init_point':'yes','print_level':3}}
+
+            ipopt_opts = {'ipopt': {'max_iter': 30,
+
+
+                                    'warm_start_init_point': 'yes', 'print_level': 0}}
+
+            solver_opts = {'ipopt_options': ipopt_opts, 'print_time': False}
+
             self.solver = nlpsol('solver', 'ipopt', prob, ipopt_opts)
+
         else:
-            qpoases_opts = {'printLevel':'low'}
-            self.solver = qpsol('solver','qpoases',prob,qpoases_opts)
+
+            qpoases_opts = {'printLevel': 'low'}
+
+            self.solver = qpsol('solver', 'qpoases', prob, qpoases_opts)
+
         self.lbw = vertcat(
-            repmat(lbx,X.size2(),1),
-            repmat(lbu,U.size2(),1))
+
+
+            repmat(lbx, X.size2(), 1),
+
+
+            repmat(lbu, U.size2(), 1))
+
         self.ubw = vertcat(
-            repmat(ubx,X.size2(),1),
-            repmat(ubu,U.size2(),1))
 
-    def set_reference(self,alt_target):
-        self.target_alt = DM.ones(self.N_pred,1) * alt_target
 
-    def set_parameters(self,spoolup_time,available_thrust,mass_loss,g):
-        self.params = DM([spoolup_time,available_thrust,mass_loss,g])
+            repmat(ubx, X.size2(), 1),
+
+
+            repmat(ubu, U.size2(), 1))
+
+    def set_reference(self, alt_target):
+
+        self.target_alt = DM.ones(self.N_pred, 1) * alt_target
+
+    def set_parameters(self, spoolup_time, available_thrust, mass_loss, g):
+
+        self.params = DM([spoolup_time, available_thrust, mass_loss, g])
 
     def warmup(self, x0):
+
         # x0: h,v,m,thrust
+
         self.w0 = vertcat(
-            reshape(DM.ones(self.ns,self.N_pred+1)*x0,self.ns*(self.N_pred+1),1),
-            reshape(DM.ones(self.nu,self.N_pred)*0,self.nu*self.N_pred,1))
-        self.sol = self.solver(x0=self.w0,lbx=self.lbw,ubx=self.ubw,lbg=self.lbg,ubg=self.ubg,p=vertcat(x0,self.params,self.target_alt))
+
+
+            reshape(DM.ones(self.ns, self.N_pred+1)
+
+
+                    * x0, self.ns*(self.N_pred+1), 1),
+
+
+            reshape(DM.ones(self.nu, self.N_pred)*0, self.nu*self.N_pred, 1))
+
+        self.sol = self.solver(x0=self.w0, lbx=self.lbw, ubx=self.ubw, lbg=self.lbg,
+
+
+                               ubg=self.ubg, p=vertcat(x0, self.params, self.target_alt))
+
         return self.sol['x'].full().flatten()[(self.N_pred+1)*self.ns]
 
-    def cycle(self,x0,time_cycle=False):
+    def cycle(self, x0, time_cycle=False):
+
         # x0: h,v,m,thrust
         start = time.time()
-        self.w0 = vertcat(x0,self.sol['x'][self.ns:])
-        self.sol = self.solver(x0=self.w0,lbx=self.lbw,ubx=self.ubw,lbg=self.lbg,ubg=self.ubg,p=vertcat(x0,self.params,self.target_alt),
-        lam_x0=self.sol['lam_x'],lam_g0=self.sol['lam_g'])
+
+        self.w0 = vertcat(x0, self.sol['x'][self.ns:])
+
+        self.sol = self.solver(x0=self.w0, lbx=self.lbw, ubx=self.ubw, lbg=self.lbg, ubg=self.ubg, p=vertcat(x0, self.params, self.target_alt),
+
+
+                               lam_x0=self.sol['lam_x'], lam_g0=self.sol['lam_g'])
+
         if time_cycle:
-            print('meas_solve_time: ',time.time() - start)
+
+            print('meas_solve_time: ', time.time() - start)
+
         return self.sol['x'].full().flatten()[(self.N_pred+1)*self.ns]
--- hover_with_mpc.py
+++ hover_with_mpc.py
@@ -2,51 +2,60 @@ import krpc
 import time
 import math
 from MPC_casadi import MPC_Casadi as HTV
-#from MPC import MPC_HoverTestVehicle as HTV
 
+# Enable Auto-accept new clients in the advanced server settings, otherwise the connection will time out
 conn = krpc.connect(name='connection')
 ves = conn.space_center.active_vessel
 ves.auto_pilot.engage()
 ves.auto_pilot.target_pitch_and_heading(90, 90)
 ut = conn.add_stream(getattr, conn.space_center, 'ut')
 altitude = conn.add_stream(getattr, ves.flight(), 'mean_altitude')
-vert_speed = conn.add_stream(getattr,ves.flight(ves.orbit.body.reference_frame), 'vertical_speed')
-mass = conn.add_stream(getattr,ves,'mass')
-air_density = conn.add_stream(getattr,ves.flight(),'atmosphere_density')
-available_thrust = conn.add_stream(getattr,ves,'available_thrust')
-thrust = conn.add_stream(getattr,ves,'thrust')
+vert_speed = conn.add_stream(getattr, ves.flight(
+    ves.orbit.body.reference_frame), 'vertical_speed')
+mass = conn.add_stream(getattr, ves, 'mass')
+air_density = conn.add_stream(getattr, ves.flight(), 'atmosphere_density')
+available_thrust = conn.add_stream(getattr, ves, 'available_thrust')
+thrust = conn.add_stream(getattr, ves, 'thrust')
 G = ves.orbit.body.gravitational_parameter
-r = conn.add_stream(getattr,ves.orbit,'radius')
+planet_radius = ves.orbit.body.equatorial_radius
+r = conn.add_stream(getattr, ves.orbit, 'radius')
 
-def g():
-    return G / r()**2
+
+def g(height, planet_radius):
+    return G / (height + planet_radius)**2
+
 
 def dm():
     before = mass()
     time.sleep(1)
     return (before - mass()) / available_thrust()
 
-def speed_from_error(error,max_speed,error_at_max_speed):
+
+def speed_from_error(error, max_speed, error_at_max_speed):
     expo = math.log(max_speed - 1) / error_at_max_speed
     if error >= 0:
         return -math.exp(error*expo) + 1
     else:
         return math.exp(-error*expo) - 1
 
+
 Tpred = 5
 Topt = 2
 intervalsize = 0.2
 
-mass_loss = 5.5*5/70000
+mass_loss = 53.98  # kg/s for Aerospike
+# mass_loss = 5.5*5/70000
 spoolup_time = 0.1
 
 target_altitude = 3000
 ves.control.activate_next_stage()
-mpc = HTV(Tpred,intervalsize,T_opt=Topt)
-mpc.set_parameters(spoolup_time,available_thrust(),mass_loss,g())
+mpc = HTV(Tpred, intervalsize, T_opt=Topt)
+mpc.set_parameters(spoolup_time, available_thrust(),
+                   mass_loss, g(altitude(), planet_radius))
 mpc.set_reference(target_altitude)
-throttle_pct = mpc.warmup([altitude(),vert_speed(),mass(),thrust()]) * 0.01
-ves.control.throttle = throttle_pct * 0.01
+throttle_pct = mpc.warmup([altitude(), vert_speed(), mass(), thrust()]) * 0.01
+ves.control.throttle = throttle_pct
+time.sleep(spoolup_time)
 ves.control.activate_next_stage()
 while True:
     start = time.time()
@@ -54,10 +63,13 @@ while True:
     alt = altitude()
     m = mass()
     thr = thrust()
-    mpc.set_parameters(0.1,available_thrust(),5.5*5/70000,g())
-    throttle_pct = mpc.cycle([alt,v,mass(),thrust()])
+
+    mpc.set_parameters(spoolup_time, available_thrust(),
+                       mass_loss, g(alt, planet_radius))
+    throttle_pct = mpc.cycle([alt, v, m, thrust()])
     ves.control.throttle = throttle_pct / 100
-    spare_time = intervalsize - (time.time() - start)
-    if  spare_time < 0:
+    time_control_end = time.time()
+    spare_time = intervalsize - (time_control_end - start)
+    if spare_time < 0:
         print('mpc took {} to long!'.format(spare_time))
     time.sleep(max(spare_time, 0))
